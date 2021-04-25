ac342cad16c68ac251c65a5157978c5063415f8e
murlord <ray12011994@gmail.com>
2021-04-25 00:47:43 -0400

    Lab3 unfinished

--- Assets/scripts/player/playerMovement.cs
+++ Assets/scripts/player/playerMovement.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+RequireComponent(typeof(Rigidbody2D), typeof(Animator))
+public class playermovement : MonoBehaviour
 {
+     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = horizontalInput.GetAxisRaw("Horizontal"); 
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
     }
 }
