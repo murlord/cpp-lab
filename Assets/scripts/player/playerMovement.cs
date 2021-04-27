c848257c5e877263f24e18eea13d603d27d31f73
murlord <ray12011994@gmail.com>
2021-04-27 18:27:55 -0400

    lab3 unfinished

--- Assets/scripts/player/playerMovement.cs
+++ Assets/scripts/player/playerMovement.cs
@@ -2,19 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-RequireComponent(typeof(Rigidbody2D), typeof(Animator))
-public class playermovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
+public class Playermovement : MonoBehaviour
 {
-     Rigidbody2D rb;
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer ninjaSprite;
+
+    public float speed;
+    public int jumpForce;
+    public bool isGrounded;
+    public LayerMask isGroundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ninjaSprite = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+
+
     }
 }
