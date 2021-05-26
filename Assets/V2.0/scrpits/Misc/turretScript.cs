47311962671b24add53119fb24f63de6b1917948
murlord <ray12011994@gmail.com>
2021-05-26 18:31:23 -0400

    lab6 unfinish

---
+++ Assets/V2.0/scrpits/Misc/turretScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretScript : MonoBehaviour
+{
+    public float Range;
+
+    public Transform Target;
+
+    bool Deteccted = false;
+
+    Vector2 Direction;
+    public GameObject Bullet;
+    public float FireRate;
+    float shootingCD = 0;
+
+    public Transform ShootPoint;
+    public float Force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPos = Target.position;
+
+        Direction = targetPos - (Vector2)transform.position;
+
+        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, Direction, Range);
+
+        if (rayInfo)
+        {
+            if (rayInfo.collider.gameObject.tag == "player")
+            {
+                if (Deteccted == false)
+                {
+                    Deteccted = true;
+                }
+            }
+            else
+            {
+                if (Deteccted == true)
+                {
+                    Deteccted = false;
+                }
+            }
+        }
+        if (Deteccted)
+        {
+            if (Time.time > shootingCD)
+            {
+                shootingCD = Time.time + 1 / FireRate;
+                shoot();
+            }
+        }
+    }
+
+    void shoot()
+    {
+       GameObject BulletIns =  Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
